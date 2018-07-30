5216fe052526f795a72ab863f1c470e89390e295
Crazy_LYL <450008553@qq.com>
2018-07-30 19:41:05 +0800

    test_mian中增加了arya

--- test_main/arya.py
+++ test_main/arya.py
@@ -4,4 +4,20 @@
 # @Author : liyongle
 # @File   : arya.py
 
-from arya
+from arya.service import warrior
+from django.utils.safestring import mark_safe
+from . import models
+
+class UserMainConfig(object):
+
+    def get_show_add(self):
+        code_list  = self.request.permission_code_list
+        if 'add' in code_list:
+            return True
+
+    def git_list_display(self):
+        code_list = self.request.permission_code_list
+        result = []
+        result.extend(self.list_display):
+        
+
